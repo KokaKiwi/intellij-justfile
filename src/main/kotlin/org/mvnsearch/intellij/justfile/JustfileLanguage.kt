3e31ade740a9037c83a9500f059cf1d6f5f05fab
linux_china <libing.chen@gmail.com>
2018-10-25 16:45:35 -0700

    Justfile language

---
+++ src/main/kotlin/org/mvnsearch/intellij/justfile/JustfileLanguage.kt
@@ -0,0 +1,11 @@
+package org.mvnsearch.intellij.justfile
+
+/**
+ * Justfile language
+ *
+ * @author linux_china
+ */
+
+import com.intellij.lang.Language
+
+object JustfileLanguage : Language("Justfile")
